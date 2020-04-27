c8851e6b86f1436f51ae3bed7ed8e104b23107fd
mohammadromin03 <63338660+mohammadromin03@users.noreply.github.com>
2020-04-27 22:20:33 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.counter').countUp();
+  
+  // navbar background color change on scroll
+
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll < 300){
+            $('.fixed-top').css('background', 'transparent');
+        } else{
+            $('.fixed-top').css('background', 'rgba(23, 162, 184, 0.9)');
+        }
+    });
+});
